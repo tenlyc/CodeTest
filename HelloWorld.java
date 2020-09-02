3c9db950d8b5f13e6000f1067d7fe37b29d6930a
Teno <66579882+tenlyc@users.noreply.github.com>
2020-09-02 10:48:42 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class HelloWorld
-{
-  public static void main(String[] args)
+{public static void main(String[] args) 
   {
-    System.out.println("Hello World!!");
-    System.out.println("I am Teno.");
+      int[] my_array = {1, 2, 5, 5, 6, 6, 7, 2, 9, 2 , 2 , 2};
+      findDupicateInArray(my_array);
+
+  }
+
+  public static void findDupicateInArray(int[] a) {
+      int count=0;
+      for(int j=0;j<a.length;j++) {
+          for(int k =j+1;k<a.length;k++) {
+              if(a[j]==a[k]) {
+                  count++;
+              }
+          }
+          if(count==1)
+             System.out.println( "重复元素 : " +  a[j] );
+          count = 0;
+      }
   }
 }
